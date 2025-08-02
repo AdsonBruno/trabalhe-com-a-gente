8a650e32309544bbdecd523828489a5ad36305f6
AdsonBruno <adsonbruno2@gmail.com>
2025-08-02 17:58:39 -0300

    refactor: ensure SearchRepositoryController.handle implement correct interface

--- api/src/presentation/controllers/searchRepository.ts
+++ api/src/presentation/controllers/searchRepository.ts
@@ -1,5 +1,7 @@
+import { HttpResponse, HttpRequest } from '../protocols/http';
+
 export class SearchRepositoryController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.query) {
       return {
         body: new Error('Missing query parameter'),
