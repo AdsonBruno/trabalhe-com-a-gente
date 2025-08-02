a10a27f3cbd9fd375c23b09d1cb3a5328925050c
AdsonBruno <adsonbruno2@gmail.com>
2025-08-02 09:07:23 -0300

    chore: add jest

---
+++ api/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+
+  testEnvironment: 'node',
+};
+
+export default config;
