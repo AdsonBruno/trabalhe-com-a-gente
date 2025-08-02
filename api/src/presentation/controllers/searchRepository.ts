7c8e8961f0baf8fa60ef1cb72f2aab2cff431d65
AdsonBruno <adsonbruno2@gmail.com>
2025-08-02 18:37:09 -0300

    refactor: move duplicated code to a helper method

--- api/src/presentation/controllers/searchRepository.ts
+++ api/src/presentation/controllers/searchRepository.ts
@@ -1,14 +1,13 @@
 import { MissingParamError } from '../errors/missingParamError';
 import { HttpResponse, HttpRequest } from '../protocols/http';
+import { badRequest } from '../helpers/httpHelper';
 
 export class SearchRepositoryController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.query) {
-      return {
-        body: new MissingParamError('query parameter'),
-        statusCode: 400,
-      };
+      return badRequest(new MissingParamError('query parameter'));
     }
+
     // Return a default success response (customize as needed)
     return {
       body: { message: 'Query received successfully.' },
