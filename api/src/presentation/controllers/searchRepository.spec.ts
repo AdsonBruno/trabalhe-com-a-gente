625681f6d03a1d05f5bed386ed11cdf55a477610
AdsonBruno <adsonbruno2@gmail.com>
2025-08-03 00:34:58 -0300

    test: ensure controller calls repository service

--- api/src/presentation/controllers/searchRepository.spec.ts
+++ api/src/presentation/controllers/searchRepository.spec.ts
@@ -1,8 +1,12 @@
 import { SearchRepositoryController } from './searchRepository';
 import { MissingParamError } from '../errors/missingParamError';
 
+const mockRepositoryService = {
+  search: jest.fn(),
+};
+
 const makeSut = (): SearchRepositoryController => {
-  return new SearchRepositoryController();
+  return new SearchRepositoryController(mockRepositoryService);
 };
 
 describe('Search Repository Controller', () => {
@@ -14,5 +18,24 @@ describe('Search Repository Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('query parameter'));
+  });
+
+  test('Should call repository service with correct parameters', () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        query: 'react',
+        page: '2',
+        per_page: '30',
+      },
+    };
+
+    sut.handle(httpRequest);
+
+    expect(mockRepositoryService.search).toHaveBeenCalledWith({
+      query: 'react',
+      page: 2,
+      perPage: 30,
+    });
   });
 });
