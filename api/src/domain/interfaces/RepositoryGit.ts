b70a2cf56f045a47dd6e681fb77de53c25fd554b
AdsonBruno <adsonbruno2@gmail.com>
2025-08-05 18:21:08 -0300

    refactor: add the most important details of each repository

--- api/src/domain/interfaces/RepositoryGit.ts
+++ api/src/domain/interfaces/RepositoryGit.ts
@@ -1,3 +1,33 @@
+export interface IOwner {
+  id: number;
+  login: string;
+  avatarUrl: string;
+}
+
+export interface IRepositoryModel {
+  id: string;
+  name: string;
+  fullName: string;
+  description: string;
+  url: string;
+  owner: IOwner;
+  stargazersCount: number;
+  watchersCount: number;
+  forksCount: number;
+  openIssuesCount: number;
+}
+
+export interface ISearchResult {
+  totalCount: number;
+  items: IRepositoryModel[];
+}
+
+export interface ISearchParams {
+  query: string;
+  page: number;
+  perPage: number;
+}
+
 export interface RepositoryGit {
-  search(params: { query: string; page: number; perPage: number }): any;
+  search(params: ISearchParams): Promise<ISearchResult>;
 }
