a6dfbf6380c526b179e2833e77fbe7b9f3006ed2
AdsonBruno <adsonbruno2@gmail.com>
2025-08-05 21:45:14 -0300

    refactor: ensures that access to GIT is asynchronous

--- api/src/presentation/controllers/searchRepository.ts
+++ api/src/presentation/controllers/searchRepository.ts
@@ -1,28 +1,33 @@
 import { MissingParamError } from '../errors/missingParamError';
 import { HttpResponse, HttpRequest } from '../protocols/http';
-import { badRequest } from '../helpers/httpHelper';
+import { badRequest, ok, serverError } from '../helpers/httpHelper';
 import { Controller } from '../protocols/controller';
 import { RepositoryGit } from '../../domain/interfaces/IRepositoryGit';
 
 export class SearchRepositoryController implements Controller {
   constructor(private readonly repositoryService: RepositoryGit) {}
 
-  handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.query) {
-      return badRequest(new MissingParamError('query parameter'));
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!httpRequest.body.query) {
+        return badRequest(new MissingParamError('query'));
+      }
+
+      const { query, page = '1', per_page = '10' } = httpRequest.body;
+
+      const result = await this.repositoryService.search({
+        query: String(query),
+        page: parseInt(String(page), 10),
+        perPage: parseInt(String(per_page), 10),
+      });
+
+      return ok(result);
+      // return {
+      //   body: { message: 'Query received successfully.' },
+      //   statusCode: 200,
+      // };}
+    } catch (error) {
+      return serverError(error);
     }
-
-    const { query, page = '1', per_page = '10' } = httpRequest.body;
-
-    this.repositoryService.search({
-      query: String(query),
-      page: parseInt(String(page), 10),
-      perPage: parseInt(String(per_page), 10),
-    });
-
-    return {
-      body: { message: 'Query received successfully.' },
-      statusCode: 200,
-    };
   }
 }
