a3860f8c85041cf7fa18427b8b685bd93301bf97
AdsonBruno <adsonbruno2@gmail.com>
2025-08-17 12:01:26 -0300

    test: should capitalize the first letter of a simple string

--- front-end-repository-searcher/src/app/features/repositories/pipes/capitalize.pipe.spec.ts
+++ front-end-repository-searcher/src/app/features/repositories/pipes/capitalize.pipe.spec.ts
@@ -5,4 +5,10 @@ describe('CapitalizePipe', () => {
     const pipe = new CapitalizePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should capitalize the first letter of a simple string', () => {
+    const pipe = new CapitalizePipe();
+
+    expect(pipe.transform('angular')).toBe('Angular');
+  });
 });
